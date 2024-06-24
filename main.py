40bc9b0e602869b341406d27fa5ed49a8fed47b0
AnshumanTiware <mr.anshumantiwari@gmail.com>
2024-06-24 14:41:14 +0200

    Add data ingestion stage

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from src.dl_project_w_mlops_cloud_deployment import logger 
+from src.dl_project_w_mlops_cloud_deployment import logger
+from dl_project_w_mlops_cloud_deployment.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom log")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
